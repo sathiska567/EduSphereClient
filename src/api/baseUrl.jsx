d014cfc5a74284def986d0fd2f16eb644c3d9f67
Chathura <cpriydasun@gmail.com>
2025-01-30 02:26:21 +0530

    update

--- src/api/baseUrl.jsx
+++ src/api/baseUrl.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const baseURL = process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_SERVER_BASE_URL;
+const baseURL = process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_SERVER_BASE_URL;
 
 
-const baseURL = "http://localhost:4000";
+// const baseURL = "http://localhost:4000";
 
 const api = axios.create({
   baseURL: `${baseURL}/api/v1`,
